e4130b84b39c8a8355552266d9389712962c7b6f
arman <armando.rico@tempedge.com>
2021-05-26 10:12:24 -0500

    formulario de solicitud de cuenta por mejorar

--- src/pages/SolicitarCuenta.js
+++ src/pages/SolicitarCuenta.js
@@ -1,9 +1,94 @@
 const SolicitarCuenta = () => {
-      return (
-            <div>
-                  <h1>SolicitarCuenta</h1>
-            </div>
-      )
-}
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="text center">
+            <form className="form-horizontal" method="post">              
+                <legend className="text-center header">Solicitud de Cuenta</legend>
+                <div className="form-group">
+                  <span className="col-md-1 col-md-offset-2 text-center">
+                    <i className="fa fa-user bigicon"></i>
+                  </span>
+                  <div className="col-md-8">
+                    <input
+                      id="fname"
+                      name="name"
+                      type="text"
+                      placeholder="First Name"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <span className="col-md-1 col-md-offset-2 text-center">
+                    <i className="fa fa-user bigicon"></i>
+                  </span>
+                  <div className="col-md-8">
+                    <input
+                      id="lname"
+                      name="name"
+                      type="text"
+                      placeholder="Last Name"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <span className="col-md-1 col-md-offset-2 text-center">
+                    <i className="fa fa-envelope-o bigicon"></i>
+                  </span>
+                  <div className="col-md-8">
+                    <input
+                      id="email"
+                      name="email"
+                      type="text"
+                      placeholder="Email Address"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <span className="col-md-1 col-md-offset-2 text-center">
+                    <i className="fa fa-phone-square bigicon"></i>
+                  </span>
+                  <div className="col-md-8">
+                    <input
+                      id="phone"
+                      name="phone"
+                      type="text"
+                      placeholder="Phone"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <span className="col-md-1 col-md-offset-2 text-center">
+                    <i className="fa fa-pencil-square-o bigicon"></i>
+                  </span>
+                  <div className="col-md-8">
+                    <textarea
+                      className="form-control"
+                      id="message"
+                      name="message"
+                      placeholder="Enter your massage for us here. We will get back to you within 2 business days."
+                      rows="7"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <div className="col-md-12 text-center">
+                    <button type="submit" className="btn btn-primary btn-lg">
+                      Submit
+                    </button>
+                  </div>
+                </div>              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SolicitarCuenta
+export default SolicitarCuenta;
